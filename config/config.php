f7c3d69f8075b5e28825a0435ed699131bfc9847
Alexander Naumov <naumov@tozen.de>
2015-12-02 16:35:40 +0100

    add example mysql query

--- config/config.php
+++ config/config.php
@@ -22,3 +22,20 @@ $GLOBALS['BE_MOD']['system']['prosearch_settings'] = array(
  * Widgets
  */
 $GLOBALS['BE_FFL']['ajaxSearchIndex'] = 'AjaxSearchIndex';
+
+/**
+ * Hooks
+ */
+$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('ProSearch', 'createOnSubmitCallback');
+
+if(TL_MODE == 'BE')
+{
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/vendor/underscore-min.js|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/vendor/zepto.min.js|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/vendor/backbone-min.js|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/ProSearch.js|static';
+}
+/**
+ * Example for Search Engine
+ * SELECT * FROM tl_prosearch_data WHERE MATCH (title, search_content) AGAINST ('*ne*' IN BOOLEAN MODE) ORDER BY tstamp, clicks DESC;
+ */
