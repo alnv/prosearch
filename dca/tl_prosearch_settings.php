86f41f4b56752a2a39a23ee5800085544ed62145
Alexander Naumov <naumov@tozen.de>
2015-12-07 17:18:22 +0100

    add test method for the new of indexing

--- dca/tl_prosearch_settings.php
+++ dca/tl_prosearch_settings.php
@@ -67,7 +67,57 @@ class tl_prosearch_settings extends ProSearch
      */
     public function ajaxSearchIndex()
     {
+
         // ajax
+        if (Input::get('index')) {
+
+            // get table
+            $tableToIndex = Input::get('index');
+            $pageNum = Input::get('page') ? (int)Input::get('page') : 1;
+            $page = 250 * $pageNum;
+            $skip = $pageNum == 1 ? 0 : $page;
+
+            // load dca
+            $this->loadDataContainer($tableToIndex);
+
+            // ckeck if dca exist
+            if(!$GLOBALS['TL_DCA'][$tableToIndex])
+            {
+                $data = array('state' => 'failure', 'table' => $tableToIndex, 'page' => $pageNum);
+                header('Content-type: application/json');
+                echo json_encode($data);
+                exit;
+            }
+
+            $dataDB = $this->Database->prepare('SELECT * FROM '.$tableToIndex.' LIMIT '.$page.' OFFSET '.$skip.'')->execute();
+
+            if($dataDB->count() < 1)
+            {
+                $data = array('state' => 'empty', 'table' => $tableToIndex, 'page' => $pageNum, 'count' => $dataDB->count());
+                header('Content-type: application/json');
+                echo json_encode($data);
+                exit;
+            }
+
+            if( $dataDB->count() >= 250 )
+            {
+                $this->saveToIndex($dataDB, $tableToIndex);
+                $data = array('state' => 'repeat', 'table' => $tableToIndex, 'page' => $pageNum, 'count' => $dataDB->count());
+                header('Content-type: application/json');
+                echo json_encode($data);
+                exit;
+            }
+
+            if( $dataDB->count() < 250 )
+            {
+                $this->saveToIndex($dataDB,$tableToIndex);
+                header('Content-type: application/json');
+                echo json_encode(array('state' => 'success', 'table' => $tableToIndex, 'page' => $pageNum, 'count' => $dataDB->count()));
+                exit;
+            }
+        }
+
+        /*
         if (strlen(Input::get('index'))) {
 
             $toIndex = Input::get('index');
@@ -125,5 +175,30 @@ class tl_prosearch_settings extends ProSearch
             echo '<li class="success">'.$toIndex.' <span>[√]</span></li>';
             exit;
         }
+        */
     }
+
+    public function saveToIndex($dataDB, $tablename)
+    {
+        $arr = array();
+
+        while($dataDB->next())
+        {
+            $data = $this->prepareIndexData($dataDB->row(), $GLOBALS['TL_DCA'][$tablename], $tablename);
+
+            if($data == false)
+            {
+                continue;
+            }
+
+            $arr[] = $data;
+
+        }
+
+        $searchDataDB = $this->Database->prepare('SELECT * FROM tl_prosearch_data WHERE dca = ?')->execute($tablename);
+        $newIndexData = $this->fillNewIndexWithExistData($searchDataDB, $arr);
+        $this->saveIndexDataIntoDB($newIndexData, $tablename);
+
+    }
+
 }
