1f506748f5375dd39ff4b505003ae79ee7c77c5d
Alexander Naumov <naumov@tozen.de>
2015-12-07 17:17:31 +0100

    add new way to index data (test)

--- assets/JsIndex.js
+++ assets/JsIndex.js
@@ -21,21 +21,59 @@
         // disable btn
         $$('.ajaxSearchIndex').setStyle('display', 'none');
 
+        //var host = window.location.origin;
+        //var path = window.location.pathname;
+        //var ajaxCall = '?do=prosearch_settings';
+        //var url = host+path+ajaxCall;
+        //var page = 1;
+
         // loop through all modules
         for(var i = 0; i < activeModules.length; i++)
         {
 
             // send request for indexing
-            new Request.Contao( {'url':window.location.href, 'followRedirects':false, onSuccess: function(li)
+            sendRequest(activeModules[i], 1);
+            /*
+            new Request.Contao( {'url': url, onSuccess: function(data)
             {
-                $$('.index_list ul').appendHTML(li);
+                //$$('.index_list ul').appendHTML(li);
+                var jsonData = JSON.parse(data);
+                console.log(jsonData);
 
             }}).get({'index':activeModules[i], 'rt':Contao.request_token});
+            */
+
 
         }
 
         return false;
 
     };
+
+
+    function sendRequest($table, $page)
+    {
+        var host = window.location.origin;
+        var path = window.location.pathname;
+        var ajaxCall = '?do=prosearch_settings';
+        var url = host+path+ajaxCall;
+        var page = $page;
+
+        new Request({'url': url, onSuccess: function(data)
+        {
+
+            var jsonData = JSON.parse(data);
+
+            if(jsonData.state == 'repeat')
+            {
+                var num = jsonData.page + 1;
+
+                return sendRequest(jsonData.table, num);
+            }
+
+
+        }}).get({'rt':Contao.request_token, 'index': $table, 'page': page});
+
+    }
 
 })();
