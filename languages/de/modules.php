e63f9644254a78701499a615b0e406a04495a09b
Alexander Naumov <naumov@tozen.de>
2015-12-08 16:10:05 +0100

    add new lang.set for de

---
+++ languages/de/modules.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Pro Search
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+$GLOBALS['TL_LANG']['MOD']['prosearch_settings'][0] = 'ProSearch';
+$GLOBALS['TL_LANG']['MOD']['prosearch_settings'][1] = 'ProSearch Einstellung';
