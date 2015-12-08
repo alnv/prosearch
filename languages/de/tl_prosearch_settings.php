9063e6d4d68ae8fdfb108a271c52dc5c01c6937e
Alexander Naumov <naumov@tozen.de>
2015-12-08 16:10:37 +0100

    add labels for prosearch settings

---
+++ languages/de/tl_prosearch_settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Pro Search
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['settings_legend']= 'Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][0] = 'Module freischalten';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][1] = 'Bitte wählen Sie aus, welche Module in die Suche aufgenommen werden sollen.';
+
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][0] = 'Suchindex aufbauen';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = 'Bitte warten Sie bis alle Balken grün geworden sind. Dieser Vorgang kann einige Minuten dauern.';
