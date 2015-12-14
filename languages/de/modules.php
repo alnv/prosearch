b023087e4a7ffbc8eac144ef6b10264211a55c12
Alexander Naumov <naumov@tozen.de>
2015-12-14 13:47:58 +0100

    add label for content element

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -12,3 +12,6 @@
 
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][0] = 'ProSearch';
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][1] = 'ProSearch Einstellung';
+
+$GLOBALS['TL_LANG']['MOD']['prosearch_content'][0] = 'Inhaltselemente';
+$GLOBALS['TL_LANG']['MOD']['prosearch_content'][1] = 'Inhaltselemente';
