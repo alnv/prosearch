b49a0a5b2ecf0a72cadd508577ab9fa61d877aa5
Alexander Naumov <naumov@tozen.de>
2015-12-14 13:47:25 +0100

    add labels for shortcuts

---
+++ languages/de/tl_prosearch_data.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Pro Search
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['top'] = 'Zuletzt Bearbeitet';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ar'] = 'Artikel';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['pa'] = 'Seitenbaum';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['abo'] = 'Newsletter Abonnenten';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fo'] = 'Formular';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['us'] = 'Benutzer';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['me'] = 'Mitglieder';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ne'] = 'Nachrichten';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ev'] = 'Veranstaltunegn';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fe'] = 'Frontend Module';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['la'] = 'Layouts';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['css'] = 'Stylesheets';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fi'] = 'Dateiverwaltung';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['si'] = 'Bildgrößen';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ce'] = 'Inhaltselemente';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fq'] = 'FAQ';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['co'] = 'Kommentare';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['nl'] = 'Newsletter';
