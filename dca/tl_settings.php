c4d7c62ffc4d9043b6d057d54094aab54a4ba693
Alexander Naumov <naumov@tozen.de>
2015-12-14 15:41:25 +0100

    add tl_settings files

---
+++ dca/tl_settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Pro Search
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
