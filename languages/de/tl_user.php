fd0d028cb9bfe7f33d27ebe595b4edc424c2a2c9
Alexander Naumov <naumov@tozen.de>
2015-12-15 16:00:24 +0100

    add labels for prosearch settings

---
+++ languages/de/tl_user.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Pro Search
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+$GLOBALS['TL_LANG']['tl_user']['prosearch_legend'] = 'ProSearch-Einstellungen';
+$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Tastaturkürzel: ProSearch öffnen', 'Hier können Sie Ihr eigenes Tastaturkürzel für Prosearch definieren. Standard: alt+space.');
