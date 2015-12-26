17a4156ba471a4d2019e42d4f54d3902da6f35c8
Alexander Naumov <alex_naumov@web.de>
2015-12-26 12:52:06 +0100

    fix legend label

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -11,5 +11,7 @@
  * @copyright 2015 Alexander Naumov
  */
 
-$GLOBALS['TL_LANG']['tl_user']['prosearch_legend'] = 'ProSearch-Einstellungen';
-$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Tastaturkürzel: ProSearch öffnen', 'Hier können Sie Ihr eigenes Tastaturkürzel für Prosearch definieren. Standard: alt+space.');
+
+$GLOBALS['TL_LANG']['tl_user']['prosearch_user_legend'] = 'ProSearch Benutzerdefiniert Einstellungen ';
+
+$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Tastaturkürzel', 'Definieren Sie Ihren Tastaturkürzel für "ProSearch öffnen" Befehl. Standard: alt+space.');
