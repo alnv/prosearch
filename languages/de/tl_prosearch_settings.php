22a6350b698461ba237e4f10d56880ddb35d8181
Alexander Naumov <alex_naumov@web.de>
2015-12-26 12:51:56 +0100

    fix de labels

--- languages/de/tl_prosearch_settings.php
+++ languages/de/tl_prosearch_settings.php
@@ -12,9 +12,15 @@
 
 
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['settings_legend']= 'Einstellungen';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['license_legend']= 'Lizenzschlüssel';
 
-$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][0] = 'Module freischalten';
-$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][1] = 'Bitte wählen Sie aus, welche Module in die Suche aufgenommen werden sollen.';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][0] = 'Tabellen in die Suche aufnehmen';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][1] = 'Bitte wählen Sie aus, welche Tabellen in die Suche aufgenommen werden sollen.';
 
-$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][0] = 'Suchindex aufbauen';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][0] = 'Suche Aufbauen';
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = 'Bitte warten Sie bis alle Balken grün geworden sind. Dieser Vorgang kann einige Minuten dauern.';
+
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][0] = 'Lizenzschlüssel eingeben';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][1] = 'Lizenzschlüssel <a href="#" target="_blank"><strong>kaufen</strong></a>';
+
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['invalidKey'] = 'Ihr Lizenzschlüssel ist ungültig';
