d9bf48c353185a23c60a8a1bb8944503ce1a493b
Alexander Naumov <alex_naumov@web.de>
2015-12-27 09:07:53 +0100

    add labels

--- dca/tl_fmodules.php
+++ dca/tl_fmodules.php
@@ -21,7 +21,7 @@ $GLOBALS['TL_DCA']['tl_fmodules']['palettes']['default'] = str_replace
   
 $GLOBALS['TL_DCA']['tl_fmodules']['fields']['ps_shortcut'] = array(
 	
-	'label' => &$GLOBALS['TL_LANG']['tl_fmodules']['ps_shortcut'],
+	'label' => &$GLOBALS['TL_LANG']['tl_prosearch_data']['ps_shortcut'],
 	'inputType' => 'text',
 	'exclude' => true,
     'eval' => array('doNotCopy' => true, 'spaceToUnderscore' => true, 'maxlength' => 32, 'tl_class' => 'w50'),
