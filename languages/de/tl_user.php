3c7fcb66a2d3e651535838de7a9010546d74e40a
Alexander Naumov <alex_naumov@web.de>
2016-01-05 18:49:41 +0100

    fix default shortkey

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -14,4 +14,4 @@
 
 $GLOBALS['TL_LANG']['tl_user']['prosearch_user_legend'] = 'ProSearch Benutzerdefiniert Einstellungen ';
 
-$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Tastaturkürzel', 'Definieren Sie Ihren Tastaturkürzel für "ProSearch öffnen" Befehl. Standard: alt+space.');
+$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Tastaturkürzel', 'Definieren Sie Ihren Tastaturkürzel für "ProSearch öffnen" Befehl. Standard: alt+m.');
--- languages/en/tl_user.php
+++ languages/en/tl_user.php
@@ -14,4 +14,4 @@
 
 $GLOBALS['TL_LANG']['tl_user']['prosearch_user_legend'] = 'ProSearch User Settings ';
 
-$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Shortcut', 'Define your custom shortcut for "start proSearch" command. Default: alt+space.');
+$GLOBALS['TL_LANG']['tl_user']['keyboard_shortcut'] = array('Shortcut', 'Define your custom shortcut for "start proSearch" command. Default: alt+m.');
