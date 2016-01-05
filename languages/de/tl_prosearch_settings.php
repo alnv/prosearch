3337a47d5e140347143fb5c8d8f66c82bb971d8f
Alexander Naumov <alex_naumov@web.de>
2016-01-05 14:43:00 +0100

    fix label

--- languages/de/tl_prosearch_settings.php
+++ languages/de/tl_prosearch_settings.php
@@ -18,7 +18,7 @@ $GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][0] = 'Tabelle
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['searchIndexModules'][1] = 'Bitte wählen Sie aus, welche Tabellen in die Suche aufgenommen werden sollen.';
 
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][0] = 'Suche Aufbauen';
-$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = 'Bitte warten Sie bis alle Balken grün geworden sind. Dieser Vorgang kann einige Minuten dauern.';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = '<strong>Bitte zuerst auf "Speichern" Klicken und danach auf "Aufbauen"</strong>. Warten Sie bitte bis alle Balken grün geworden sind. Dieser Vorgang kann einige Minuten dauern.';
 
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][0] = 'Lizenzschlüssel eingeben';
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][1] = 'Lizenzschlüssel <a href="#" target="_blank"><strong>kaufen</strong></a>';
