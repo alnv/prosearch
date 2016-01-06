499927211f4a9c412a08e4a23d08cc2751af35ea
Alexander Naumov <alex_naumov@web.de>
2016-01-06 16:14:30 +0100

    fix path to js and css resources

--- src/Resources/contao/widgets/TagTextField.php
+++ src/Resources/contao/widgets/TagTextField.php
@@ -61,9 +61,9 @@ class TagTextField extends Widget
             $this->removeTags($tags);
         }
 
-        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/vendor/mootagify.js|static';
-        $GLOBALS['TL_CSS'][] = 'system/modules/prosearch/assets/css/mootagify-bootstrap.css|static';
-        $GLOBALS['TL_CSS'][] = 'system/modules/prosearch/assets/css/mootagify.css|static';
+        $GLOBALS['TL_JAVASCRIPT'][] = $GLOBALS['PS_PUBLIC_PATH'].'vendor/mootagify.js|static';
+        $GLOBALS['TL_CSS'][] = $GLOBALS['PS_PUBLIC_PATH'].'css/mootagify-bootstrap.css|static';
+        $GLOBALS['TL_CSS'][] = $GLOBALS['PS_PUBLIC_PATH'].'css/mootagify.css|static';
 
         $options = $this->options ? $this->options : array();
 
