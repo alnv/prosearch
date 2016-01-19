93c8119e2fca5408602883522127c887f75b6e3f
Alexander Naumov <naumov@tozen.de>
2016-01-19 14:43:43 +0100

    rename tl_content meta name

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -13,5 +13,5 @@
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][0] = 'ProSearch';
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][1] = 'ProSearch Einstellung';
 
-$GLOBALS['TL_LANG']['MOD']['prosearch_content'][0] = 'Inhaltselemente';
-$GLOBALS['TL_LANG']['MOD']['prosearch_content'][1] = 'Inhaltselemente';
+$GLOBALS['TL_LANG']['MOD']['ps_content'][0] = 'Inhaltselemente';
+$GLOBALS['TL_LANG']['MOD']['ps_content'][1] = 'Inhaltselemente';
--- languages/en/modules.php
+++ languages/en/modules.php
@@ -13,5 +13,5 @@
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][0] = 'ProSearch';
 $GLOBALS['TL_LANG']['MOD']['prosearch_settings'][1] = 'ProSearch Settings';
 
-$GLOBALS['TL_LANG']['MOD']['prosearch_content'][0] = 'Content types';
-$GLOBALS['TL_LANG']['MOD']['prosearch_content'][1] = 'Content types';
+$GLOBALS['TL_LANG']['MOD']['ps_content'][0] = 'Content types';
+$GLOBALS['TL_LANG']['MOD']['ps_content'][1] = 'Content types';
