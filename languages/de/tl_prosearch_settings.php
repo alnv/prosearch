fa3037c0fbe68abcbd5310670ff9879134f091cd
Alexander Naumov <naumov@tozen.de>
2016-02-16 16:35:57 +0100

    add label for addDescriptionToSearchContent checkbox

--- languages/de/tl_prosearch_settings.php
+++ languages/de/tl_prosearch_settings.php
@@ -23,4 +23,7 @@ $GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = '<strong>Bitte 
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][0] = 'Lizenzschlüssel eingeben';
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][1] = 'Lizenzschlüssel <a href="#" target="_blank"><strong>kaufen</strong></a>';
 
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['addDescriptionToSearchContent'][0] = 'Texte in die Suche aufnehmen';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['addDescriptionToSearchContent'][1] = 'Diese Option kann die Suche verlangsamen, dafür werden Text Spalten indiziert.';
+
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['invalidKey'] = 'Ihr Lizenzschlüssel ist ungültig';
--- languages/en/tl_prosearch_settings.php
+++ languages/en/tl_prosearch_settings.php
@@ -23,4 +23,7 @@ $GLOBALS['TL_LANG']['tl_prosearch_settings']['createIndex'][1] = 'Please wait un
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][0] = 'Please enter valid license key';
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['prosearchLicense'][1] = '<a href="#" target="_blank"><strong>Buy</strong></a> license key.';
 
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['addDescriptionToSearchContent'][0] = 'Add text to Search';
+$GLOBALS['TL_LANG']['tl_prosearch_settings']['addDescriptionToSearchContent'][1] = 'This Option can slow down the search.';
+
 $GLOBALS['TL_LANG']['tl_prosearch_settings']['invalidKey'] = 'Your license key is invalid.';
