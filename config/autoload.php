d2aff0137dd8d01846b8cbcd8ac34d6bb97bb23e
Alexander Naumov <naumov@tozen.de>
2016-04-08 08:55:36 +0200

    fix internal cache problem

--- config/autoload.php
+++ config/autoload.php
@@ -12,24 +12,23 @@
  */
 
 // namespace
-\Contao\ClassLoader::addNamespace('ProSearch');
+ClassLoader::addNamespace('ProSearch');
 
-$path = 'system/modules/prosearch/';
+$proSearchPath = 'system/modules/prosearch/';
+
 if( (version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) )
 {
-    $path = 'vendor/prosearch/prosearch/';
+    $proSearchPath = 'vendor/prosearch/prosearch/';
 }
 
 // load classes
 ClassLoader::addClasses(array(
-
-    'ProSearch\ProSearch' => $path.'src/Resources/contao/classes/ProSearch.php',
-    'ProSearch\Helper' => $path.'src/Resources/contao/classes/Helper.php',
-    'ProSearch\UserSettings' => $path.'src/Resources/contao/classes/UserSettings.php',
-    'ProSearch\ProSearchPalette' => $path.'src/Resources/contao/classes/ProSearchPalette.php',
-    'ProSearch\PrepareDataException' => $path.'src/Resources/contao/classes/PrepareDataException.php',
-    'ProSearch\ProSearchDataContainer' => $path.'src/Resources/contao/classes/ProSearchDataContainer.php',
-    'ProSearch\AjaxSearchIndex' => $path.'src/Resources/contao/widgets/AjaxSearchIndex.php',
-    'ProSearch\TagTextField' => $path.'src/Resources/contao/widgets/TagTextField.php'
-
+    'ProSearch\ProSearch' => $proSearchPath.'src/Resources/contao/classes/ProSearch.php',
+    'ProSearch\Helper' => $proSearchPath.'src/Resources/contao/classes/Helper.php',
+    'ProSearch\UserSettings' => $proSearchPath.'src/Resources/contao/classes/UserSettings.php',
+    'ProSearch\ProSearchPalette' => $proSearchPath.'src/Resources/contao/classes/ProSearchPalette.php',
+    'ProSearch\PrepareDataException' => $proSearchPath.'src/Resources/contao/classes/PrepareDataException.php',
+    'ProSearch\ProSearchDataContainer' => $proSearchPath.'src/Resources/contao/classes/ProSearchDataContainer.php',
+    'ProSearch\AjaxSearchIndex' => $proSearchPath.'src/Resources/contao/widgets/AjaxSearchIndex.php',
+    'ProSearch\TagTextField' => $proSearchPath.'src/Resources/contao/widgets/TagTextField.php'
 ));
