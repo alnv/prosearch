6b510252716a09b03f424d3be947437762dfd906
Alexander Naumov <me@alexandernaumov.de>
2017-02-27 16:04:51 +0100

    add class

--- config/autoload.php
+++ config/autoload.php
@@ -11,24 +11,24 @@
  * @copyright 2016 Alexander Naumov
  */
 
-// namespace
 ClassLoader::addNamespace('ProSearch');
 
-$proSearchPath = 'system/modules/prosearch/';
+$strProSearchPath = 'system/modules/prosearch/';
 
-if( (version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) )
-{
-    $proSearchPath = 'vendor/prosearch/prosearch/';
+if( (version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) ) {
+
+    $strProSearchPath = 'vendor/prosearch/prosearch/';
 }
 
-// load classes
-ClassLoader::addClasses(array(
-    'ProSearch\ProSearch' => $proSearchPath.'src/Resources/contao/classes/ProSearch.php',
-    'ProSearch\Helper' => $proSearchPath.'src/Resources/contao/classes/Helper.php',
-    'ProSearch\UserSettings' => $proSearchPath.'src/Resources/contao/classes/UserSettings.php',
-    'ProSearch\ProSearchPalette' => $proSearchPath.'src/Resources/contao/classes/ProSearchPalette.php',
-    'ProSearch\PrepareDataException' => $proSearchPath.'src/Resources/contao/classes/PrepareDataException.php',
-    'ProSearch\ProSearchDataContainer' => $proSearchPath.'src/Resources/contao/classes/ProSearchDataContainer.php',
-    'ProSearch\AjaxSearchIndex' => $proSearchPath.'src/Resources/contao/widgets/AjaxSearchIndex.php',
-    'ProSearch\TagTextField' => $proSearchPath.'src/Resources/contao/widgets/TagTextField.php'
-));
+ClassLoader::addClasses([
+
+    'ProSearch\ProSearch' => $strProSearchPath . 'src/Resources/contao/classes/ProSearch.php',
+    'ProSearch\Helper' => $strProSearchPath . 'src/Resources/contao/classes/Helper.php',
+    'ProSearch\UserSettings' => $strProSearchPath . 'src/Resources/contao/classes/UserSettings.php',
+    'ProSearch\ProSearchPalette' => $strProSearchPath . 'src/Resources/contao/classes/ProSearchPalette.php',
+    'ProSearch\PrepareDataException' => $strProSearchPath . 'src/Resources/contao/classes/PrepareDataException.php',
+    'ProSearch\ProSearchDataContainer' => $strProSearchPath . 'src/Resources/contao/classes/ProSearchDataContainer.php',
+    'ProSearch\AjaxSearchIndex' => $strProSearchPath . 'src/Resources/contao/widgets/AjaxSearchIndex.php',
+    'ProSearch\TagTextField' => $strProSearchPath . 'src/Resources/contao/widgets/TagTextField.php',
+    'ProSearch\InitializeProSearch' => $strProSearchPath . 'src/Resources/contao/classes/InitializeProSearch.php'
+]);
