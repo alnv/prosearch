072b493ae0236e41a06207ea36b1806bea0d0fb8
Alexander Naumov <me@alexandernaumov.de>
2017-02-27 16:04:25 +0100

    set script placeholder

---
+++ src/Resources/contao/classes/InitializeProSearch.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProSearch;
+
+class InitializeProSearch {
+
+
+    private $strPlaceholder = '<!-- ### %PROSEARCH_SCRIPT_TAG% ### ->';
+
+
+    public function setScriptPlaceholder() {
+
+        $GLOBALS['TL_MOOTOOLS'][] = $this->strPlaceholder;
+    }
+}
