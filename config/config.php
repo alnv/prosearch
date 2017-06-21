fc340a7bfae510c60c032a45d9139c11201bd00c
Alexander Naumov <me@alexandernaumov.de>
2017-06-21 15:28:36 +0200

    improve be item link

--- config/config.php
+++ config/config.php
@@ -1,28 +1,9 @@
 <?php
 
-/**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2016 Leo Feyer
- *
- * @package   ProSearch
- * @author    Alexander Naumov http://www.alexandernaumov.de
- * @license   CC BY-NC-ND 4.0
- * @copyright 2016 Alexander Naumov
- */
-
-$GLOBALS['PS_PUBLIC_PATH'] = 'system/modules/prosearch/assets/';
-
-if ( ( version_compare(VERSION, '4.0', '>=') && !$GLOBALS['PS_NO_COMPOSER'] && $GLOBALS['PS_NO_COMPOSER'] != true ) ) {
-
-    $GLOBALS['PS_PUBLIC_PATH'] = 'bundles/prosearch/';
-}
-
-
 $GLOBALS['BE_MOD']['system']['prosearch_settings'] = [
 
-    'tables' => [ 'tl_prosearch_settings', 'tl_prosearch_data' ],
-    'icon' => $GLOBALS['PS_PUBLIC_PATH'] . 'icon.png',
+    'icon' => 'system/modules/prosearch/assets/icon.png',
+    'tables' => [ 'tl_prosearch_settings', 'tl_prosearch_data' ]
 ];
 
 $GLOBALS['BE_FFL']['ajaxSearchIndex'] = 'AjaxSearchIndex';
@@ -35,29 +16,37 @@ $GLOBALS['TL_HOOKS']['initializeSystem'][] = [ 'InitializeProSearch', 'setScript
 
 if (TL_MODE == 'BE') {
     
-    $GLOBALS['TL_CSS'][] = $GLOBALS['PS_PUBLIC_PATH'] . 'css/theme.css|static';
-    $GLOBALS['TL_JAVASCRIPT'][] = $GLOBALS['PS_PUBLIC_PATH'] . 'vendor/underscore-min.js|static';
-    
-    $strProSearchHeader = '<span id="openProSearch" class="header_prosearch_container"><a href="#!prosearch" class="header_prosearch prosearch_backend_icon" title="ProSearch | Backend-Suche für Contao" >ProSearch</a></span>';
-    
+    $GLOBALS['TL_CSS'][] = 'system/modules/prosearch/assets/css/theme.css|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/vendor/underscore-min.js|static';
+
+    $strButton = '<li id="%s" class="%s"><a href="%s" class="%s" title="%s">%s</a></li>';
+
+    $strText = 'ProSearch';
+    $strId = 'openProSearch';
+    $strHref = '#!prosearch';
+    $strIcon = 'prosearch_v3icon';
+    $strItemCssClass = 'header_prosearch';
+    $strContainerCssClass =  'header_prosearch_container';
+    $strTitle = $GLOBALS['TL_LANG']['MSC']['prosearchTitle'];
+
     if ( version_compare( VERSION, '4.2', '>=' ) ) {
-    
-        $strProSearchHeader = '<li id="openProSearch" class="header_prosearch_container"><a href="#!prosearch" class="header_prosearch" title="ProSearch | Backend-Suche für Contao" ><img src="' . $GLOBALS['PS_PUBLIC_PATH'] . 'prosearch.svg" width="18" height="18" alt="ProSearch | Backend-Suche für Contao" title=""></a></li>';
+
+        $strIcon = 'prosearch_v4icon';
     }
     
-    $GLOBALS['TL_MOOTOOLS'][] = "<script>var strProSearchHeaderTemplate = '$strProSearchHeader';</script>";
-    $GLOBALS['TL_JAVASCRIPT'][] = $GLOBALS['PS_PUBLIC_PATH'] . 'ProSearch.js|static';
+    $strButton = sprintf( $strButton,
+
+        $strId,
+        $strContainerCssClass,
+        $strHref,
+        $strItemCssClass . ' ' . $strIcon,
+        $strTitle,
+        $strText
+    );
+    
+    $GLOBALS['TL_MOOTOOLS'][] = "<script>var strProSearchHeaderTemplate = '$strButton';</script>";
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/ProSearch.js|static';
 }
 
-$GLOBALS['PS_EDITABLE_FILES'] = explode(',', (\Contao\Config::get('editableFiles')));
-
-$license = \Contao\Config::get('prosearchLicense');
-
-if (!isset($license) || !in_array(md5($license), ProSearch\Helper::$validSums, true)) {
-
-    if (TL_MODE == 'BE') {
-
-        $GLOBALS['TL_MOOTOOLS'][] = '<script>var validLicense = false;</script>';
-    }
-}
+$GLOBALS['PS_EDITABLE_FILES'] = explode( ',', ( \Config::get('editableFiles') ) );
 
