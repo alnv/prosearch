d8cf176c7b705c67418dfc6b4701cbfe5b56c569
Alexander Naumov <me@alexandernaumov.de>
2017-06-21 15:23:16 +0200

    change JsIndex path

--- src/Resources/contao/widgets/AjaxSearchIndex.php
+++ src/Resources/contao/widgets/AjaxSearchIndex.php
@@ -1,16 +1,5 @@
 <?php namespace ProSearch;
 
-/**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2016 Leo Feyer
- *
- * @package   ProSearch
- * @author    Alexander Naumov http://www.alexandernaumov.de
- * @license   CC BY-NC-ND 4.0
- * @copyright 2016 Alexander Naumov
- */
-
 use Contao\Config;
 use Contao\Widget;
 
@@ -43,7 +32,7 @@ class AjaxSearchIndex extends Widget
         $GLOBALS['TL_MOOTOOLS'][] = '<script>var proSearchActiveModules = '.$json.';</script>';
 
         // load js
-        $GLOBALS['TL_JAVASCRIPT'][] = $GLOBALS['PS_PUBLIC_PATH'].'JsIndex.js|static';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/prosearch/assets/JsIndex.js|static';
 
         return  '<div class="index_list"><ul class="ul"></ul></div><div class="ajaxSearchIndex"><a class="tl_submit" style="margin-bottom: 5px; margin-top: 5px" onclick="Backend.getScrollOffset();return AjaxRequest.ajaxSearchIndex()">'.$bStr.'</a></div>';
 
